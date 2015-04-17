7954263a1d12d69f0a99baf0abb0d6011f67c083
Chris Zonca <Chrismco923@gmail.com>
2015-04-17 11:39:14 -0400

    Add a way to specify matches without needing to have a PostgreSQL database defined.

--- modules/matches.js
+++ modules/matches.js
@@ -4,6 +4,7 @@
  * @author Chris Zonca
  */
 var pg = require('pg');
+var fs = require('fs');
 
 // The currently cached pool of match ids
 var cachedMatches = [];
@@ -12,23 +13,34 @@ var cachedMatches = [];
  * Gets and caches a list of known match ids from the database.
  */
 function retrieveMatches() {
-    pg.connect(process.env.DATABASE_URL, function (err, client) {
-        if (!err) {
-            var query = client.query("SELECT * FROM match_ids", null, function(err) {
-                if (err) {
-                    console.log("Could not retrieve match list");
-                    console.log(err);
-                }
-                client.end();
-            });
-            query.on('row', function(row) {
-                cachedMatches.push(row.match_id);
-            })
-        } else {
-            console.log("An error occurred connecting to the database. No matches available!");
-            console.log(err);
+
+    // Prioritize the database URL, if it is defined
+    if (process.env.DATABASE_URL) {
+        pg.connect(process.env.DATABASE_URL, function (err, client) {
+            if (!err) {
+                var query = client.query("SELECT * FROM match_ids", null, function (err) {
+                    if (err) {
+                        console.log("Could not retrieve match list");
+                        console.log(err);
+                    }
+                    client.end();
+                });
+                query.on('row', function (row) {
+                    cachedMatches.push(row.match_id);
+                })
+            } else {
+                console.log("An error occurred connecting to the database. No matches available!");
+                console.log(err);
+            }
+        });
+    } else {
+        try {
+            cachedMatches = JSON.parse(fs.readFileSync(".matches"));
+        } catch (ex) {
+            console.log("Could not load matches, no matches will be available!");
+            console.log(ex);
         }
-    });
+    }
 }
 
 retrieveMatches();
